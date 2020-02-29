7bac637937b208873624aa323ee2f73d0be65362
Ankit Shahu <ankitstar00786@gmail.com>
2020-02-29 14:25:07 +0530

    done with nav bar

--- js/main.js
+++ js/main.js
@@ -1,20 +1,40 @@
 const navSlide = () =>{
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-link');
-    // const navLink = document.querySelector('.nav-link li')
+    const navLink = document.querySelectorAll('.nav-link li')
 
     burger.addEventListener('click',()=>{
-        nav.classList.toggle('.nav-active');
+        nav.classList.toggle('nav-active');
 
-        // navLink.forEach((link, index) => {
-        //     if(link.style.animation){
-        //         link.style.animation = '';
-        //     }else{
-        //         link.style.animation = `navLinkFade 0.5s ease forwaords ${index / 7 +0.3}s` ;
-        //     } 
-        //   });
+        navLink.forEach((link, index) => {
+            if(link.style.animation){
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwaords ${index / 7 + 1.5}s` ;
+            } 
+          });
+
+        burger.classList.toggle('toggle');
     });
     
  }
 
 navSlide();
+
+
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
